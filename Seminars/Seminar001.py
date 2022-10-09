99968899be491dce1ef112b05e6f96bcd533c2f7
Artem <22ilden07@gmail.com>
2022-10-09 18:49:08 +0700

    Seminar 001

--- Seminars/Seminar001.py
+++ Seminars/Seminar001.py
@@ -1,42 +1,42 @@
-# 1. Напишите программу, которая принимает на вход два 
+# Задача №1. Напишите программу, которая принимает на вход два 
 # числа и проверяет, является ли одно число квадратом другого.
 
 # num1 = int(input("Введите первое число: "))
 # num2 = int(input("Введите второе число: "))
 
 # if num1 == num2*num2 or num2 == num1*num1:
-#     print("Одно число является квадратом другого числа")
+#      print("Одно число является квадратом другого числа")
 # else:
-#     print("Одно число не является квадратом другого числа")
+#      print("Одно число не является квадратом другого числа")
 
 # try:
-#     number1 = int(input('Введите первое число: '))
-#     number2 = int(input('Введите второе число: '))
-#     if number1 ** 2 == number2:
-#         print(f'{number2} является квадратом числа {number1}')
-#     elif number2 ** 2 == number1:
-#         print(f'{number1} является квадратом числа {number2}')
-#     else:
-#         print('Ни одно из числе не является квадратом другого')
+#      number1 = int(input('Введите первое число: '))
+#      number2 = int(input('Введите второе число: '))
+#      if number1 ** 2 == number2:
+#          print(f'{number2} является квадратом числа {number1}')
+#      elif number2 ** 2 == number1:
+#          print(f'{number1} является квадратом числа {number2}')
+#      else:
+#          print('Ни одно из числе не является квадратом другого')
 # except:
-#     print('Введите целое число')
+#      print('Введите целое число')
 
 
-# 2. Напишите программу, которая на вход принимает 5 чисел 
+# Задача №2. Напишите программу, которая на вход принимает 5 чисел 
 # и находит максимальное из них.
 
 # def create_list():
-#     array = []
-#     for i in range(5):
-#         value = int(input("Введите число: "))
-#         array.append(value)
-#     return array
+#      array = []
+#      for i in range(5):
+#          value = int(input("Введите число: "))
+#          array.append(value)
+#      return array
 # def find_max(array):
-#     max = array[0]
-#     for i in range(len(array)):
-#         if array[i]>max:
-#             max = array[i]
-#     return max
+#      max = array[0]
+#      for i in range(len(array)):
+#          if array[i]>max:
+#              max = array[i]
+#      return max
 
 # res = create_list()
 # print(res)
@@ -44,53 +44,51 @@
 # print(f"Максимальное значение в списке равно {max}")
 
 
-# 1. Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N
+# Задача №3 . Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N
 
 # def digits (num):
-#     for i in range(-num, num + 1):
-#         print(i, end=' ')   
+#      for i in range(-num, num + 1):
+#          print(i, end=' ')   
 # try:
-#     current = int(input("Введите число: "))
-#     digits(current)
+#      current = int(input("Введите число: "))
+#      digits(current)
 
 # except:
-#     print('Введите целое число')
+#      print('Введите целое число')
 
-# 2. Напишите программу, которая будет принимать на вход дробь и показывать первую цифру дробной части числа.
+# Задача №4. Напишите программу, которая будет принимать на вход дробь и показывать первую цифру дробной части числа.
 
 # def find_first_number(number):
-#     res = number
-#     number *= 10
-#     res = int(number % 10)
-#     return res
+#      res = number
+#      number *= 10
+#      res = int(number % 10)
+#      return res
+# try:
+#      num = float(input("Введите дробное число: "))
+#      print(find_first_number(num))
+# except:
+#      print('Введите дробное число')
 
 # try:
-#     num = float(input("Введите число: "))
-#     print(find_first_number(num))
+#      x = float(input('enter x:'))
+#      if int(x) - x == 0:
+#          print('no')    
+#      else:
+#          print(int(x * 10 % 10))
 # except:
-#     print('Введите целое число')
+#      print('enter numbers')
 
 
+
+# Задача №5. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 5 и 10 или 15, но не 30.
+
+# def multiple(num):
+#      if (num % 5 == 0 and num % 10 == 0 or num % 15 == 0) and num % 30 != 0:
+#          print("Число удовлетворяет данным условиям")
+#      else:
+#          print("Число не удовлетворяет данным условиям")
 # try:
-#     x = float(input('enter x:'))
-#     if int(x) - x == 0:
-#         print('no')    
-#     else:
-#         print(int(x * 10 % 10))
+#      num = int(input("Введите число: "))
+#      multiple(num)
 # except:
-#     print('enter numbers')
-
-
-
-# 3. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 5 и 10 или 15, но не 30.
-
-def multiple(num):
-    if (num % 5 == 0 and num % 10 == 0 or num % 15 == 0) and num % 30 != 0:
-        print("Число удовлетворяет данным условиям")
-    else:
-        print("Число не удовлетворяет данным условиям")
-try:
-    num = int(input("Введите число: "))
-    multiple(num)
-except:
-    print('Введите целое число')
+#      print('Введите целое число')
