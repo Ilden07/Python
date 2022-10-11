97920f9d7c8e576bbf320e98c595e776b6de45de
Artem <22ilden07@gmail.com>
2022-10-11 10:05:47 +0700

    HomeWork001 Task №3,Task  №4,Task  №5

---
+++ HomeWorks001/Task№4.py
@@ -0,0 +1,67 @@
+# Задача №4 HARD необязательнаяНапишите простой калькулятор, который считывает с пользовательского ввода три строки: 
+# первое число, второе число и операцию, после чего применяет операцию к введённым числам 
+# ("первое число" "операция" "второе число") и выводит результат на экран.
+
+# Поддерживаемые операции: +, -, /, *, mod, pow, div, где
+# mod — это взятие остатка от деления,
+# pow — возведение в степень,
+# div — целочисленное деление.
+
+# Если выполняется деление и второе число равно 0, необходимо выводить строку "Деление на 0!".
+
+# Обратите внимание, что на вход программе приходят вещественные числа.
+
+# Sample Input 1:
+# 5.0
+# 0.0
+# mod
+# Sample Output 1:
+# Деление на 0!
+
+# Sample Input 2:
+# -12.0
+# -8.0
+# *
+# Sample Output 2:
+# 96.0
+
+# Sample Input 3:
+# 5.0
+# 10.0
+# /
+# Sample Output 3:
+# 0.5
+def calc (input_data):
+    if input_data[2] == '+':
+        return input_data[0] + input_data[1]
+    if input_data[2] == '-':
+        return input_data[0] - input_data[1]
+    if input_data[2] == '/':
+        if input_data[1] !=0:
+            return input_data[0] / input_data[1]
+        else:
+            print('ОШИБКА! Деление на 0!')
+    if input_data[2] == '*':
+        return input_data[0] * input_data[1]
+    if input_data[2] == 'mod':
+        return input_data[0] % input_data[1]
+    if input_data[2] == 'pow':
+        return input_data[0] ** input_data[1]
+    if input_data[2] == 'div':
+        if input_data[1] !=0:
+            return input_data[0] // input_data[1]
+        else:
+            print('ОШИБКА! Деление на 0!')
+
+try:
+    print('Калькулятор! Введите 2 числа и операцию!')
+    input_num = [0,0,0]
+    operation = ['+', '-', '/', '*', 'mod', 'pow', 'div']
+    while input_num[2] not in operation:
+        input_num[0] = float(input('Введите первое число: '))
+        input_num[1] = float(input('Введите второе число: '))
+        print('Поддерживаемые операции: +, -, /, *, mod, pow, div, где \nmod — это взятие остатка от деления, \npow — возведение в степень, \ndiv — целочисленное деление.')
+        input_num[2] = input('Введите операцию: ')
+    print(f'{input_num[0]} {input_num[2]} {input_num[1]} = {calc(input_num)}')
+except:
+    print('Введите число!')
